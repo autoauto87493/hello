14eaaa8b6f6c6a643889a3e2f6e17b95d9be613a
autoauto87493 <autoauto87493@gmail.com>
2025-01-05 23:39:01 +0530

    help desk is full of comments

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Header =()=>{
+    return(
+        <div id = "hed">
+            <h1 style={{backgroundColor:"red"}}>header</h1>
+        </div>
+    )
+}
+
+export default Header; 
